a1f65e9a88b605229873cf90c555a35f92bb5fa7
bloooder <bloooder@gmail.com>
2015-06-15 17:49:17 +0200

    Ver 1.00!!!

--- src/pl/rsiblabla/prim/Graph.java
+++ src/pl/rsiblabla/prim/Graph.java
@@ -1,7 +1,11 @@
 package pl.rsiblabla.prim;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Random;
+
+import javax.naming.LimitExceededException;
 
 public class Graph implements Serializable {
 	private static final long serialVersionUID = -1045936618203421854L;
@@ -29,18 +33,58 @@ public class Graph implements Serializable {
 	public static Graph generateGraph(int nodes, int maxLinksPerNode, int maxWeight) {
 		Random rand = new Random(System.currentTimeMillis());
 		Graph newGraph = new Graph();
+		
 		newGraph.nodes = new GraphNode[nodes];
-		for (int i = 0; i < nodes; i++) {
-			GraphNode newNode = new GraphNode();
-			for (int j = 0; j < newNode.links.size(); j++) {
-				GraphLink newLink = new GraphLink();
-				newLink.destinationNodeNr = rand.nextInt(nodes);
-				newLink.weight = rand.nextInt(maxWeight);
-				newNode.links.add(newLink);
+		for (int i = 0; i < nodes; i++)
+			newGraph.nodes[i] = new GraphNode();
+		
+		LinkedList<Integer> nodeStack = new LinkedList<Integer>();
+		ArrayList<Integer> lostNodes = new ArrayList<Integer>(nodes);
+		for (int i = 0; i < nodes; i++)
+			lostNodes.add(i);
+		
+		int firstNode = rand.nextInt(nodes);
+		nodeStack.addFirst(firstNode);
+		lostNodes.remove((Integer)firstNode);
+		
+		while(nodeStack.size() != 0) {
+			int actualIndex = nodeStack.removeFirst();
+			GraphNode node = newGraph.nodes[actualIndex];
+			int linkAmount = rand.nextInt(maxLinksPerNode) + 1;
+			
+			for (int i = 0; i < linkAmount; i++) {
+				int destNodeIndex = 0;
+				if(lostNodes.size() == 0) {
+					boolean goodIndex = false;
+					while(!goodIndex) {
+						goodIndex = true;
+						destNodeIndex = rand.nextInt(nodes);
+						if(destNodeIndex == actualIndex) {
+							goodIndex = false;
+							continue;
+						}
+						for(GraphLink link : node.links)
+							if(link.destinationNodeNr == destNodeIndex) {
+								goodIndex = false;
+								break;
+							}
+					}
+				} else {
+					destNodeIndex = lostNodes.remove(rand.nextInt(lostNodes.size()));
+					nodeStack.addLast(destNodeIndex);
+				}
+				
+				GraphLink link = new GraphLink();
+				link.destinationNodeNr = destNodeIndex;
+				link.weight = rand.nextInt(maxWeight);
+				node.links.add(link);
+				
+				GraphLink symmetricLink = new GraphLink();
+				symmetricLink.destinationNodeNr = actualIndex;
+				symmetricLink.weight = link.weight;
+				newGraph.nodes[destNodeIndex].links.add(symmetricLink);
 			}
-			newGraph.nodes[i] = newNode;
 		}
-		//TODO: search for alone nodes and connect them
 		return newGraph;
 	}
 }
